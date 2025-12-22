a33882338fada08b4d6b382f6b5935f5ff01f5e0
Mohammad Khan <khanmo2k@gmail.com>
2025-12-22 17:46:49 -0500

    Add task fetching with loading state

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,35 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [tasks, setTasks] = useState([])
+  const [selectedTask, setSelectedTask] = useState(null)
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    async function fetchTasks() {
+      const res = await fetch("https://jsonplaceholder.typicode.com/todos")
+      const data = await res.json()
+      setTasks(data)
+      setLoading(false)
+    }
+
+    fetchTasks()
+  }, [])
 
   return (
     <>
       <h1>Task Insight Dashboard</h1>
+      {loading ? (
+        <p>Loading users...</p>
+      ) : (
+        <ul>
+          <li>Placeholder list</li>
+          <li>There are {tasks.length} tasks to do</li>
+        </ul>
+      )}
     </>
   )
 }
